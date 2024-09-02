2c48e5ef887cfa7374563dd9ed54c8e7608d6b1e
Zhengliang Wang <puck19970418@gmail.com>
2024-09-02 02:52:59 -0400

    add sample rest endpoint on getting youtube caption directly

--- youtube-service/app.py
+++ youtube-service/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from youtube_caption_client import YoutubeCaptionClient
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/rest/captions/<videoId>')
+def getYoutubeCaption(videoId):
+    print(videoId)
+    transcript = YoutubeCaptionClient.get_transcript(videoId)
+    return transcript
 
 if __name__ == '__main__':
     app.run(debug=True)
